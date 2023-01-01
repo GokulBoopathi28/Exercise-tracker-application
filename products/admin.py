37752cb13b2bfb375bde3f1893b46c71c16c4af6
gokulboopathi18 <87892768+gokulboopathi18@users.noreply.github.com>
2023-01-01 20:38:35 +0530

    done

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product,Offer
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display=('name','stock','price')
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display=('code','description','discount')
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Offer,OfferAdmin)
